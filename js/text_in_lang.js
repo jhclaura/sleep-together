d2c017a770b6251fac520e0d1b9db96302166c54
jhclaura <linkinmonkey@gmail.com>
2017-12-25 16:29:01 -0500

    test

--- js/text_in_lang.js
+++ js/text_in_lang.js
@@ -2,211 +2,211 @@ var langSwap = {
 	"credit": {
 		"en": "",
 		"fr": "",
-		"de": "<p style="font-size: 1.3em;">/// SEHR SEHR KURZ ///</p>
-		        <p>Das Leben ist kurz. Mehr als je zuvor. Wir wollen alles. Und wir wollen es jetzt. Sehr Sehr Kurz ist eine Serie aus 10 interaktiven Projekten, bei denen das Thema Mobilität jeweils in 60 Sekunden behandelt wird. Eine Koproduktion von National Film Board of Canada und ARTE, in Zusammenarbeit mit IDFA DocLab.</p>
-		        <br>
-	    		<p style="font-size: 1.3em;">/// AB IN DIE KISTE ///</p>
-		        <p>Ab in die Kiste, eine WebVR-Mehrbenutzer-Erlebnis für Menschen, die einschlafen möchten. Gestaltet von Laura Juo-Hsin Chen, koproduziert von NFB und ARTE.</p>
-		        <br>
-		        <p style="font-size: 1.3em;">/// KREDITE ///</p>
-		        <b><ins>SCHAFFUNG</ins></b>
-	            <br>
-	            <br>
-	            <b>Gestaltung</b>
-	            <br>
-	            Laura Juo-Hsin Chen
-	            <br>
-	            <br>
-	            <b>Tonaufnahme</b>
-	            <br>
-	            Andy Sigler
-	            <br>
-	            <br>
-	            <b><ins>ONF / NFB</ins></b>
-	            <br>
-	            <br>
-	            <b>Produktionsausführung</b>
-	            <br>
-	            Hugues Sweeney
-	            <br>
-	            <br>
-	            <b>Produktionsmanagement</b>
-	            <br>
-	            Marie-Pier Gauthier
-	            <br>
-	            Nathalie Bédard Morin
-	            <br>
-	            <br>
-	            <b>Redaktionsleitung</b>
-	            <br>
-	            Valérie Darveau
-	            <br>
-	            <br>
-	            <b>Technische Leitung</b>
-	            <br>
-	            Martin Viau
-	            <br>
-	            <br>
-	            <b>Koordination</b>
-	            <br>
-	            Caroline Fournier
-	            <br>
-	            Claudia Boutin
-	            <br>
-	            Dominique Brunet
-	            <br>
-	            <br>
-	            <b>Verwaltung</b>
-	            <br>
-	            Marie-Andrée Bonneau
-	            <br>
-	            <br>
-	            <b>Management Marketing</b>
-	            <br>
-	            Tammy Peddle
-	            <br>
-	            <br>
-	            <b>Koordination Marketing</b>
-	            <br>
-	            Florent Prevelle
-	            <br>
-	            <br>
-	            <b>Strategie Soziale Medien</b>
-	            <br>
-	            Kate Ruscito
-	            <br>
-	            <br>
-	            <b>Management Web-Content</b>
-	            <br>
-	            Félix-Antoine Viens
-	            <br>
-	            <br>
-	            <b>Pressearbeit</b>
-	            <br>
-	            Marie-Claude Lamoureux
-	            <br>
-	            Pat Dillon
-	            <br>
-	            <br>
-	            <b>IT</b>
-	            <br>
-	            Sergiu Suciu
-	            <br>
-	            <br>
-	            <b>Rechtsberatung</b>
-	            <br>
-	            Peter Kallianiotis
-	            <br>
-	            <br>
-	            <b><ins>ARTE France</ins></b>
-	            <br>
-	            <br>
-	            <b>Leitung Pôle Web</b>
-	            <br>
-	            Gilles Freissinier
-	            <br>
-	            <br>
-	            <b>Leitung Webproduktionen</b>
-	            <br>
-	            Marianne Levy-Leblon
-	            <br>
-	            <br>
-	            <b>Programmverantwortliche</b>
-	            <br>
-	            Marie Berthoumieu
-	            <br>
-	            Emilie Bessard
-	            <br>
-	            <br>
-	            <b>Projektmanagement</b>
-	            <br>
-	            Stéphane Nauroy
-	            <br>
-	            <br>
-	            <b>Leitung Soziale Medien</b>
-	            <br>
-	            Léa Jagut
-	            <br>
-	            <br>
-	            <b>Administrator</b>
-	            <br>
-	            Jerôme Vernet
-	            <br>
-	            <br>
-	            <b>ARTE France Développement</b>
-	            <br>
-	            <b>Produktionsleitung</b>
-	            <br>
-	            Claire Aubret
-	            <br>
-	            Angèle Le Névé
-	            <br>
-	            <b>Assistenz</b>
-	            <br>
-	            Juliette Droillard
-	            <br>
-	            <br>
-	            <b>Übersetzung</b>
-	            <br>
-	            Christian Stonner
-	            <br>
-	            <br>
-	            <b>Verantwortlicher ARTE Studio Lab</b>
-	            <br>
-	            Clément Dhamelincourt
-	            <br>
-	            <b>Technischer Leiter</b>
-	            <br>
-	            Vincent Chagny
-	            <br>
-	            <b>Projektmanager</b>
-	            <br>
-	            Noé Daudin-Clavaud
-	            <br>
-	            <br>
-	            <b><ins>IDFA DocLab</ins></b>
-	            <br>
-	            <br>
-	            <b>Ausschreibung</b>
-	            <br>
-	            Caspar Sonnen
-				<br>
-				Wotienke Vermeer
-				<br>
-				Michael Zbieranowski
-				<br>
-	            <br>
-	            <b><ins>Dank An</ins></b>
-	            <br>
-	            <br>
-				Ziv Schneider - Künstlerin Neue Medien
-				<br>
-				Jouke Vuurmans - Media Monks
-				<br>
-				Julia Kaganskiy - New Museum
-				<br>
-				Myriam Achard - PHI Center
-				<br>
-				Florent Maurin - The Pixel Hunt
-				<br>
-				Jepchumba - Digitaler Künstler und Kurator
-				<br>
-				Rafaella Wang
-				<br>
-				Zina Kisch
-	            <br>
-	            <br>
-	            Sam Lavigne für die Idee aus Useless Press: Daily Life VR
-	            <br>
-	            Brandon Jones und
-	            <br>Vladimir Vukicevic für den Entwurf der WebVR
-	            <br>
-	            Boris Smus für die Polyfills und den Boilerplate-Code der WebVR
-	            <br>
-	            Ricardo Cabello für THREE.js
-	            <br>
-	            <br>
-	            <br>
+		"de": "<p style='font-size: 1.3em;'>/// SEHR SEHR KURZ ///</p>\
+		        <p>Das Leben ist kurz. Mehr als je zuvor. Wir wollen alles. Und wir wollen es jetzt. Sehr Sehr Kurz ist eine Serie aus 10 interaktiven Projekten, bei denen das Thema Mobilität jeweils in 60 Sekunden behandelt wird. Eine Koproduktion von National Film Board of Canada und ARTE, in Zusammenarbeit mit IDFA DocLab.</p>\
+		        <br>\
+	    		<p style="font-size: 1.3em;">/// AB IN DIE KISTE ///</p>\
+		        <p>Ab in die Kiste, eine WebVR-Mehrbenutzer-Erlebnis für Menschen, die einschlafen möchten. Gestaltet von Laura Juo-Hsin Chen, koproduziert von NFB und ARTE.</p>\
+		        <br>\
+		        <p style="font-size: 1.3em;">/// KREDITE ///</p>\
+		        <b><ins>SCHAFFUNG</ins></b>\
+	            <br>\
+	            <br>\
+	            <b>Gestaltung</b>\
+	            <br>\
+	            Laura Juo-Hsin Chen\
+	            <br>\
+	            <br>\
+	            <b>Tonaufnahme</b>\
+	            <br>\
+	            Andy Sigler\
+	            <br>\
+	            <br>\
+	            <b><ins>ONF / NFB</ins></b>\
+	            <br>\
+	            <br>\
+	            <b>Produktionsausführung</b>\
+	            <br>\
+	            Hugues Sweeney\
+	            <br>\
+	            <br>\
+	            <b>Produktionsmanagement</b>\
+	            <br>\
+	            Marie-Pier Gauthier\
+	            <br>\
+	            Nathalie Bédard Morin\
+	            <br>\
+	            <br>\
+	            <b>Redaktionsleitung</b>\
+	            <br>\
+	            Valérie Darveau\
+	            <br>\
+	            <br>\
+	            <b>Technische Leitung</b>\
+	            <br>\
+	            Martin Viau\
+	            <br>\
+	            <br>\
+	            <b>Koordination</b>\
+	            <br>\
+	            Caroline Fournier\
+	            <br>\
+	            Claudia Boutin\
+	            <br>\
+	            Dominique Brunet\
+	            <br>\
+	            <br>\
+	            <b>Verwaltung</b>\
+	            <br>\
+	            Marie-Andrée Bonneau\
+	            <br>\
+	            <br>\
+	            <b>Management Marketing</b>\
+	            <br>\
+	            Tammy Peddle\
+	            <br>\
+	            <br>\
+	            <b>Koordination Marketing</b>\
+	            <br>\
+	            Florent Prevelle\
+	            <br>\
+	            <br>\
+	            <b>Strategie Soziale Medien</b>\
+	            <br>\
+	            Kate Ruscito\
+	            <br>\
+	            <br>\
+	            <b>Management Web-Content</b>\
+	            <br>\
+	            Félix-Antoine Viens\
+	            <br>\
+	            <br>\
+	            <b>Pressearbeit</b>\
+	            <br>\
+	            Marie-Claude Lamoureux\
+	            <br>\
+	            Pat Dillon\
+	            <br>\
+	            <br>\
+	            <b>IT</b>\
+	            <br>\
+	            Sergiu Suciu\
+	            <br>\
+	            <br>\
+	            <b>Rechtsberatung</b>\
+	            <br>\
+	            Peter Kallianiotis\
+	            <br>\
+	            <br>\
+	            <b><ins>ARTE France</ins></b>\
+	            <br>\
+	            <br>\
+	            <b>Leitung Pôle Web</b>\
+	            <br>\
+	            Gilles Freissinier\
+	            <br>\
+	            <br>\
+	            <b>Leitung Webproduktionen</b>\
+	            <br>\
+	            Marianne Levy-Leblon\
+	            <br>\
+	            <br>\
+	            <b>Programmverantwortliche</b>\
+	            <br>\
+	            Marie Berthoumieu\
+	            <br>\
+	            Emilie Bessard\
+	            <br>\
+	            <br>\
+	            <b>Projektmanagement</b>\
+	            <br>\
+	            Stéphane Nauroy\
+	            <br>\
+	            <br>\
+	            <b>Leitung Soziale Medien</b>\
+	            <br>\
+	            Léa Jagut\
+	            <br>\
+	            <br>\
+	            <b>Administrator</b>\
+	            <br>\
+	            Jerôme Vernet\
+	            <br>\
+	            <br>\
+	            <b>ARTE France Développement</b>\
+	            <br>\
+	            <b>Produktionsleitung</b>\
+	            <br>\
+	            Claire Aubret\
+	            <br>\
+	            Angèle Le Névé\
+	            <br>\
+	            <b>Assistenz</b>\
+	            <br>\
+	            Juliette Droillard\
+	            <br>\
+	            <br>\
+	            <b>Übersetzung</b>\
+	            <br>\
+	            Christian Stonner\
+	            <br>\
+	            <br>\
+	            <b>Verantwortlicher ARTE Studio Lab</b>\
+	            <br>\
+	            Clément Dhamelincourt\
+	            <br>\
+	            <b>Technischer Leiter</b>\
+	            <br>\
+	            Vincent Chagny\
+	            <br>\
+	            <b>Projektmanager</b>\
+	            <br>\
+	            Noé Daudin-Clavaud\
+	            <br>\
+	            <br>\
+	            <b><ins>IDFA DocLab</ins></b>\
+	            <br>\
+	            <br>\
+	            <b>Ausschreibung</b>\
+	            <br>\
+	            Caspar Sonnen\
+				<br>\
+				Wotienke Vermeer\
+				<br>\
+				Michael Zbieranowski\
+				<br>\
+	            <br>\
+	            <b><ins>Dank An</ins></b>\
+	            <br>\
+	            <br>\
+				Ziv Schneider - Künstlerin Neue Medien\
+				<br>\
+				Jouke Vuurmans - Media Monks\
+				<br>\
+				Julia Kaganskiy - New Museum\
+				<br>\
+				Myriam Achard - PHI Center\
+				<br>\
+				Florent Maurin - The Pixel Hunt\
+				<br>\
+				Jepchumba - Digitaler Künstler und Kurator\
+				<br>\
+				Rafaella Wang\
+				<br>\
+				Zina Kisch\
+	            <br>\
+	            <br>\
+	            Sam Lavigne für die Idee aus Useless Press: Daily Life VR\
+	            <br>\
+	            Brandon Jones und\
+	            <br>Vladimir Vukicevic für den Entwurf der WebVR\
+	            <br>\
+	            Boris Smus für die Polyfills und den Boilerplate-Code der WebVR\
+	            <br>\
+	            Ricardo Cabello für THREE.js\
+	            <br>\
+	            <br>\
+	            <br>\
 	            © ARTE  FRANCE - ONF 2017"
 	}
 };
